c3af5060b0bb5e85e00a8aee6eb31c98ce1419d5
Borys Lykah <lykahb@fastmail.com>
2023-03-17 15:09:17 -0600

    Update URL of the schema

--- gen.sh
+++ gen.sh
@@ -10,7 +10,7 @@ if [ "$TAG" = "" ]; then
     exit 1
 fi
 
-curl --fail https://raw.githubusercontent.com/ory/hydra/master/docs/versioned_docs/version-$TAG/.static/api.json > api.swagger.json
+curl --fail https://raw.githubusercontent.com/ory/hydra/$TAG/spec/api.json > api.swagger.json
 
 openapi-generator-cli generate -i api.swagger.json -g haskell-http-client -o $OUT -c config.yml
 
